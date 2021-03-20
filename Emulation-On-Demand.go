28c2fbbfa1ed741e0f6ac0085c9503ce2b279eae
Jacobu <jacob.arlo.leibeck@gmail.com>
2021-03-20 19:24:39 -0400

    [RetroArch-Launch] Runs installed Retroarch help in cmd

--- Emulation-On-Demand.go
+++ Emulation-On-Demand.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "os/exec"
+  "runtime"
+  "strings"
+)
 
 func main() {
-    fmt.Println("Initial Go Script.")
+
+	bRunSetup := false
+	sOS := runtime.GOOS
+	
+	
+	//Main Script
+	
+	if bRunSetup == true {
+		//First Time Setup------------------------------------------------------
+
+	
+	} else {
+		//Launcher--------------------------------------------------------------
+		if sOS == "windows" {
+			
+			reader := bufio.NewReader(os.Stdin)
+			
+			c := exec.Command("cmd", "/C", "retroarch --help",)
+			stdout, err := c.Output()
+			
+			if err != nil {
+				fmt.Println("Error")
+			}
+
+			fmt.Print(string(stdout))
+			
+			fmt.Print("-> ")
+			text, _ := reader.ReadString('\n')
+			// convert CRLF to LF
+			text = strings.Replace(text, "\n", "", -1)
+
+			if strings.Compare("hi", text) == 0 {
+			  fmt.Println("hello, Yourself")
+			}
+	
+
+		}else{
+			fmt.Println("ERROR: Unsupported Operating System. Only Windows is Supported.")
+			os.Exit(1)
+		}
+	}
+	
+		
 }
