0e249635dba35eef2e5e0df301f55b713fd25ecc
Jacobu <jacob.arlo.leibeck@gmail.com>
2021-03-20 22:19:23 -0400

    [RetroArch-Launch] Move to Laptop.

--- Emulation-On-Demand.go
+++ Emulation-On-Demand.go
@@ -6,13 +6,68 @@ import (
   "os"
   "os/exec"
   "runtime"
-  "strings"
+  //"strings"
 )
+
+var bDebug bool = true
+
+func retroarch_LaunchGame(sPath string, sFlags string, bPrintOutput bool){
+
+	if runtime.GOOS == "windows" {
+		if bDebug==true {
+			fmt.Println("DEBUG: Running Retroarch command:")
+			fmt.Println("\t> "+sPath+" "+sFlags)
+		}
+		c := exec.Command(sPath+" "+sFlags,)
+		stdout, err := c.Output()
+	
+
+	
+		if err != nil {
+			fmt.Println("ERROR: Failed to run RetroArch command.")
+			if string(stdout) != "" {
+			
+				fmt.Println("***Begin ERROR Log***")	
+				fmt.Print(string(stdout))
+				fmt.Println("***End ERROR Log***")
+			}else {
+				fmt.Println("ERROR: No Error was returned...")
+			}
+		}
+		
+		if bPrintOutput == true {
+			fmt.Print(string(stdout))
+		}	
+	}
+}
+
+func cmd_WaitForNewline(){
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Press ENTER to Continue.")
+	reader.ReadString('\n')
+}
+
+
 
 func main() {
 
 	bRunSetup := false
 	sOS := runtime.GOOS
+	
+	//Initialize Dir variables
+	sDir_Default_Install := ""
+	
+	if sOS == "windows" {
+		sDir_Default_Install = "\"C\\:Program Files\\RetroArch\\"
+	
+	}else{
+		fmt.Println("ERROR: Unsupported Operating System. Only Windows is Supported.")
+		os.Exit(1)
+	}	
+	
+	
+	
+
 	
 	
 	//Main Script
@@ -24,27 +79,13 @@ func main() {
 	} else {
 		//Launcher--------------------------------------------------------------
 		if sOS == "windows" {
+			sPath_RetroArch := sDir_Default_Install + "retroarch.exe" + "\""
+			sArgs_RetroArch := "--help"
 			
-			reader := bufio.NewReader(os.Stdin)
-			
-			c := exec.Command("cmd", "/C", "retroarch --help",)
-			stdout, err := c.Output()
-			
-			if err != nil {
-				fmt.Println("Error")
-			}
 
-			fmt.Print(string(stdout))
-			
-			fmt.Print("-> ")
-			text, _ := reader.ReadString('\n')
-			// convert CRLF to LF
-			text = strings.Replace(text, "\n", "", -1)
+			retroarch_LaunchGame(sPath_RetroArch, sArgs_RetroArch, true)
 
-			if strings.Compare("hi", text) == 0 {
-			  fmt.Println("hello, Yourself")
-			}
-	
+			cmd_WaitForNewline()
 
 		}else{
 			fmt.Println("ERROR: Unsupported Operating System. Only Windows is Supported.")
